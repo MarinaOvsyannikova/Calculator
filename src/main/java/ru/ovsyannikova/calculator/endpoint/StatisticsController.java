64f98f9fbb8c4a59c2ddfedb5577d63c4c48af3f
Ovsyannikova Marina <meliannaelf@gmail.com>
2017-10-23 04:02:20 +0600

    Divided controllers for calculation and statistics

--- src/main/java/ru/ovsyannikova/calculator/endpoint/StatisticsController.java
+++ src/main/java/ru/ovsyannikova/calculator/endpoint/StatisticsController.java
@@ -2,55 +2,27 @@ package ru.ovsyannikova.calculator.endpoint;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import ru.ovsyannikova.calculator.domain.entity.EvaluationTask;
 import ru.ovsyannikova.calculator.dto.request.DateDto;
-import ru.ovsyannikova.calculator.dto.request.EvaluationTaskDto;
 import ru.ovsyannikova.calculator.dto.request.OperationDto;
-import ru.ovsyannikova.calculator.dto.response.*;
-import ru.ovsyannikova.calculator.service.EvaluationResult;
-import ru.ovsyannikova.calculator.service.EvaluationResultService;
+import ru.ovsyannikova.calculator.dto.response.EvaluationTasksResponse;
+import ru.ovsyannikova.calculator.dto.response.NumberCounterDetailsResponse;
+import ru.ovsyannikova.calculator.dto.response.TasksCountResponse;
 import ru.ovsyannikova.calculator.service.EvaluationTaskService;
 import ru.ovsyannikova.calculator.service.NumberCounterService;
 
-import java.io.IOException;
-
-@RestController
-public class EvaluationTaskController {
+@Controller
+public class StatisticsController {
     private EvaluationTaskService taskService;
-    private EvaluationResultService resultService;
     private NumberCounterService counterService;
 
     @Autowired
-    public EvaluationTaskController(EvaluationTaskService taskService, EvaluationResultService resultService, NumberCounterService counterService) {
+    public StatisticsController(EvaluationTaskService taskService, NumberCounterService counterService) {
         this.taskService = taskService;
-        this.resultService = resultService;
         this.counterService = counterService;
     }
-
-//    @RequestMapping(value = "/calculations", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-//    public EvaluationTasksResponse getAllTasks() {
-//        Iterable<EvaluationTask> evaluationTaskList = taskService.getAllTasks();
-//        EvaluationTasksResponse response = new EvaluationTasksResponse(evaluationTaskList);
-//        return response;
-//    }
-
-    @RequestMapping(value = "/calculate", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public EvaluationTaskDetailsResponse createNewRecord(EvaluationTaskDto request) throws IOException {
-        EvaluationResult evaluationResult = taskService.evaluate(request.getTask());
-        resultService.collectData(evaluationResult);
-        EvaluationTaskDetailsResponse response = new EvaluationTaskDetailsResponse(evaluationResult.getResult());
-        return response;
-    }
-
-//    @RequestMapping(value = "/numbers", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-//    public NumberCounterResponse showNumbers() {
-//        Iterable<NumberCounter> numberCounterList = numberCounterRepository.findAll();
-//        NumberCounterResponse response = new NumberCounterResponse(numberCounterList);
-//        return response;
-//    }
 
     @RequestMapping(value = "/count", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     public TasksCountResponse countTaskByDate(DateDto request) {
